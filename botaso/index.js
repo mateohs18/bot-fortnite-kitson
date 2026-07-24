728f83599b58bdbe8f2b1c2bdbbd1fe31d6a747d
mateohs18 <spinmhs@gmail.com>
2026-07-23 23:26:24 -0300

    Update index.js

--- botaso/index.js
+++ botaso/index.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
-const { Client } = require('fnbr');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: '*', methods: ['GET', 'POST', 'OPTIONS'] }));
-
 const bots = [];
 
 // ==========================================================
